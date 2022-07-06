2250f745897bf959a7a81758f03ba88b8554d4ec
sentiSehzzada <79615571+sentiSehzzada@users.noreply.github.com>
2022-07-06 09:03:02 +0530

    Update heap_sort.c

--- heap_sort.c
+++ heap_sort.c
@@ -2,7 +2,7 @@
   
   #include <stdio.h>
   
-  // Function to swap the the position of two elements
+  // swap function new way
   void swap(int *a, int *b) {
     int temp = *a;
     *a = *b;
@@ -10,7 +10,7 @@
   }
   
   void heapify(int arr[], int n, int i) {
-    // Find largest among root, left child and right child
+    // baap, beta, beti me sabse bada khojna hai
     int largest = i;
     int left = 2 * i + 1;
     int right = 2 * i + 2;
@@ -21,14 +21,12 @@
     if (right < n && arr[right] > arr[largest])
       largest = right;
   
-    // Swap and continue heapifying if root is not largest
+    // agar baap bada nhi hai to
     if (largest != i) {
       swap(&arr[i], &arr[largest]);
       heapify(arr, n, largest);
     }
   }
-  
-  // Main function to do heap sort
   void heapSort(int arr[], int n) {
     // Build max heap
     for (int i = n / 2 - 1; i >= 0; i--)
@@ -37,20 +35,15 @@
     // Heap sort
     for (int i = n - 1; i >= 0; i--) {
       swap(&arr[0], &arr[i]);
-  
-      // Heapify root element to get highest element at root again
+  //root element yani baap ko heapify krna k liye
       heapify(arr, i, 0);
     }
   }
-  
-  // Print an array
   void printArray(int arr[], int n) {
     for (int i = 0; i < n; ++i)
       printf("%d ", arr[i]);
     printf("\n");
   }
-  
-  // Driver code
   int main() {
     int arr[100],n;
     printf("how many elements are there in tha array :");
